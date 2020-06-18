359f95414e1d59d5714a50a9ca6913b7be6dde80
Tristan Elliott <tristanelliott7@gmail.com>
2020-06-18 17:42:41 -0300

    adding NavBar

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 
 import NavBar from '../NavBar';
 
+import './index.css'
+
+
 
 
 
--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
-import Word from '../../AnimatedComponents/AnimatedNavBar';
+import './index.css'
 
-import './index.css';
 
 const NavBar = ()=>{
+
   return(
-    <div className="navbar-container">
-      <div className="weather">
-        <Word/>
+    <div className="nav-header">
+      <div className="nav-container">
+      <h1 className="logo"> Logo</h1>
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/">About</Link></li>
+            <li><Link to="/">Contact</Link></li>
+            <li><Link to="/">Info</Link></li>
+          </ul>
+        </nav>
       </div>
     </div>
   )
